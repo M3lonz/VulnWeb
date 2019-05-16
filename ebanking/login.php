3fcdd56086b9ed388d4fa0a1ec6ba2805ed8db99
M3lonz <50399843+M3lonz@users.noreply.github.com>
2019-05-16 17:11:19 +0700

    Add files via upload

---
+++ ebanking/login.php
@@ -0,0 +1,7 @@
+<?php
+    //require_once('lib/nusoap.php');
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    echo $email." ".$password;
+    echo <meta http-equiv="refresh" content="0;URL='accout.html'" /> 
+?>
